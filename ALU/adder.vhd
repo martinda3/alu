eb61250944cd416046c04719ceb6222247b4c58a
dajion <martinda3@vcu.edu>
2019-10-07 14:26:38 -0400

    to pc

--- ALU/adder.vhd
+++ ALU/adder.vhd
@@ -1,49 +1,34 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use std.textio.all;
 
 ENTITY ADDER is
-  PORT(A     :  IN  std_logic := '0';
-       B     :  IN  std_logic := '0';
+  PORT(A     :  IN  std_logic_vector(31 downto 0) := "00000000000000000000000000000001";
+	   B     :  IN  std_logic_vector(31 downto 0) := "00000000000000000000000000000001";
        CIN   :  IN  std_logic := '0';
        COUT  : OUT  std_logic := '0';
-       SUM   : OUT  std_logic := '0';
+	   SUM   : OUT  std_logic_vector(31 downto 0) := "00000000000000000000000000000000"
        );
 END ADDER;
 
 ARCHITECTURE behavior OF ADDER IS
-  
-  signal DATA1 : STD_LOGIC_VECTOR(31 DOWNTO 0):= "00000000000000000000000000000000";
-  signal DATA2 : STD_LOGIC_VECTOR(31 DOWNTO 0):= "00000000000000000000000000000000";
-  
+	
+  SIGNAL CARRY : std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
   BEGIN
 
-    PROCESS(EN)
-      BEGIN
-        if (EN = '1') Then
-          case ALUctr is
-            when "000" => -- ADDER
-              ReadData1 <= R0;
-            when "001" => -- SUBBTRACTER
-              ReadData1 <= R1;
-            when "010" => -- AND
-              ReadData1 <= R2;
-            when "011" => -- OR
-              ReadData1 <= R3;
-            when "100" => -- LOGIC SHIFT LEFT
-              ReadData1 <= R4;
-            when "101" => -- LOGIC SHIFT RIGHT
-              ReadData1 <= R5;
-            when "110" => -- ARITHMETIC SHIFT LEFT
-              ReadData1 <= R6;
-            when "111" => -- ARITHMETIC SHIFT RIGHT
-              ReadData1 <= R7;
-            when others =>
-            end case;
-      else
-        
-      end if; 
-    END PROCESS;
-    
+    PROCESS(A, B, CIN, CARRY)
+	  variable li : line;	  
+	  BEGIN
+		SUM(0) <= A(0) XOR B(0) XOR CIN;
+		CARRY(0) <= (A(0) AND CIN) OR (B(0) AND CIN) OR (A(0) AND B(0));
+		FOR i IN 1 TO 31 LOOP
+		  SUM(i) <= A(i) XOR B(i) XOR CARRY(i-1);
+		  CARRY(i) <= (A(i) AND CARRY(i-1)) OR (B(i) AND CARRY(i-1)) OR (A(i) AND B(i));
+		END LOOP;
+		COUT <= CARRY(31);
+		write (li, to_hstring(SUM));
+		WRITELINE(OUTPUT, li);
+	END PROCESS;
 	 
 END behavior;
 
