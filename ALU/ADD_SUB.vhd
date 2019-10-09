8fbced42593f08cd062bec3e7c0ed8438c698ce8
Dajion Martin <martinda3@vcu.edu>
2019-10-08 22:16:45 -0400

    taking a breakand new direction

--- ALU/ADD_SUB.vhd
+++ ALU/ADD_SUB.vhd
@@ -2,22 +2,22 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY ADD_SUB IS
-  PORT(A  :   IN  STD_LOGIC := '0';
-	   B  :   IN  STD_LOGIC := '0';
-       CI :   IN  STD_LOGIC := '0';
-       S  :  OUT  STD_LOGIC := '0';
-       CO :  OUT  STD_LOGIC := '0');
+  PORT(A  :   IN  STD_LOGIC := 'U';
+	     B  :   IN  STD_LOGIC := 'U';
+       CI :   IN  STD_LOGIC := 'U';
+       S  :  OUT  STD_LOGIC := 'U';
+       CO :  OUT  STD_LOGIC := 'U');
 END ADD_SUB;
 
 ARCHITECTURE BEHAVIOR OF ADD_SUB IS
 	
   BEGIN
     PROCESS(A, B, CI)
-    VARIABLE B_NEW : STD_LOGIC := '0';
+    VARIABLE B_NEW : STD_LOGIC := 'U';
 	  BEGIN
 	    B_NEW := B XOR CI;
-		S <= A XOR B_NEW XOR CI;
-		CO <= (A AND CI) OR (B_NEW AND CI) OR (A AND B_NEW);
+		  S <= A XOR B_NEW XOR CI;
+		  CO <= (A AND CI) OR (B_NEW AND CI) OR (A AND B_NEW);
 	END PROCESS;
 END BEHAVIOR;
 
